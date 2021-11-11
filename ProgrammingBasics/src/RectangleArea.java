487a1a38a71b47feace2ca17d84335abdf8ac023
SvetoslavaTs <svetoslavatsvetkova@gmail.com>
2021-11-11 12:50:01 +0200

    Add first course of SoftUni program.

---
+++ ProgrammingBasics/src/RectangleArea.java
@@ -0,0 +1,18 @@
+public class RectangleArea {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 7;
+        int area = a * b;
+        System.out.println(area);
+        int c = 7;
+        int d = 8;
+        int area1 = c * d;
+        System.out.println(area1);
+        int i = 12;
+        int f = 5;
+        int area2 = i * f;
+        System.out.println(area2);
+    }
+
+}
