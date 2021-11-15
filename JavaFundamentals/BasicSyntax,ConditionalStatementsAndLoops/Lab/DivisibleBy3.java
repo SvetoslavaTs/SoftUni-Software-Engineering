7e28d3d99bbbdabd46a9f577a3b5c534e242c881
SvetoslavaTs <svetoslavatsvetkova@gmail.com>
2021-11-15 10:12:52 +0200

    Basic Syntax, Conditional Statements and Loops, Data Types and Variables, Arrays, Methods, Lists, Objects and Classes, Associative Arrays, Text Processing, Regular Expressions.

--- JavaFundamentals/BasicSyntax,ConditionalStatementsAndLoops/Lab/DivisibleBy3.java
+++
@@ -1,9 +0,0 @@
-package LabBasicSyntaxConditionalStatementsAndLoops;
-
-public class DivisibleBy3 {
-    public static void main(String[] args) {
-        for (int i = 3; i <= 100; i += 3) {
-            System.out.println(i);
-        }
-    }
-}
